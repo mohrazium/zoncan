f0d0cdfbb93f0b02f077bf2815398ff5617e0fa8
Mohammad Hadi Rafiei Zadeh <mhrz.dev@gmail.com>
2022-07-31 19:00:33 +0430

    ZON-3 : published cryptography methods to cryptology package on pub.dev then added to deps.

--- lib/security/security.dart
+++ lib/security/security.dart
@@ -2,5 +2,4 @@ library zoncan.security;
 
 export 'src/data/data.dart';
 export 'src/domain/domain.dart';
-export 'src/cryptography/cryptography.dart';
 export 'src/application/application.dart';
--- lib/security/src/cryptography/cryptography.dart
+++
@@ -1,7 +0,0 @@
-library zoncan.security.cryptography;
-
-import 'dart:typed_data';
-
-import 'core/core.dart' show CryptoHelpers, CryptoHelpersString, PBKDF2, PasswordCryptology;
-
-part 'password/password_encryption.dart';
